825f7a863da77257607a2d828774d38ff835debc
Alain ZAMBONI <az@unistra.fr>
2026-02-18 12:34:59 +0100

    Added tenant_id label no neutron quota

--- exporters/exporter_test.go
+++ exporters/exporter_test.go
@@ -70,7 +70,6 @@ var fixtures map[string]string = map[string]string{
 	"/compute/limits?tenant_id=5961c443439d4fcebe42643723755e9d": "nova_os_limits",
 	"/compute/limits?tenant_id=bab7d5c60cd041a0a36f7c4b6e1dd978": "nova_os_limits",
 	"/compute/limits?tenant_id=fdb8424c4e4f4c0ba32c52e2de3bd80e": "nova_os_limits",
-	"/compute/limits?tenant_id=bab7d5c60cd041a0a36f7c4b6e1dd978": "nova_os_limits",
 	"/compute/servers/detail?all_tenants=true":                   "nova_os_servers",
 	"/compute/os-simple-tenant-usage?detailed=1":                 "nova_os_simple_tenant_usage",
 	"/glance/":          "glance_api_discovery",
